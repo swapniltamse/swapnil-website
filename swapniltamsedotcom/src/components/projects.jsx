0083e70b9b1c1cfc11b39647b1c507f2c3814b6a
Swapnil <stamsepublic@gmail.com>
2025-02-09 14:21:07 -0500

    add projects

---
+++ swapniltamsedotcom/src/components/projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>My Projects</h2>
+      <p>Here are a few projects I've worked on:</p>
+      <ul>
+        <li>Project 1: Description</li>
+        <li>Project 2: Description</li>
+        <li>Project 3: Description</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Projects;
