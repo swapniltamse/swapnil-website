6af6f9aaf28a0a1d5d811595a81d843a4d4e36d1
Swapnil <stamsepublic@gmail.com>
2025-02-09 14:18:18 -0500

    add header

---
+++ swapniltamsedotcom/src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Swapnil Tamse</h1>
+      <nav>
+        <a href="#bio">About Me</a>
+        <a href="#projects">Projects</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
